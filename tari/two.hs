ee9f1bf71af299eda1b59998a08c6a5ac54fb2c9
Taricag <ulfa2sukma6@gmailcom>
2015-09-05 19:05:29 +0700

    change

--- tari/two.hs
+++ tari/two.hs
@@ -2,24 +2,38 @@ module Two where
 
 import Data.List
 
-gaya m a = a*m
+--1.a
 
 take' 0 _=[]
 take' _ []=[]
 take' n (x:xs)= x : take' (n-1) (xs)
 
+--pembatas
+
 drop' 0 (x:xs) = (x:xs)
 drop' _ [] = []
 drop' n (x:xs) = drop' (n-1) (xs)
 
+--pembatas
+
 fst' (a,b) = a
+
+--pembatas
 
 snd' (a,b) = b
 
-delete' _ [] = []
-delete' 
+--pembatas
 
 sqr x= x^2
 
+--pembatas
+
 map' f (x:xs)= (f x) : map' f (xs)
 map' f[]= []
+
+--pembatas
+
+delete' n (x:xs)
+  |n==x = xs
+  |n/=x = x : delete' n(xs)
+    |n/=x = (x:xs)
